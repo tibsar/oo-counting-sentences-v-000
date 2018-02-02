bbb066fe31be2b5774d9f792e69ca5950e90545a
Sara Tibbetts <sxt4866@rit.edu>
2018-02-02 16:19:00 +0000

    automatically backed up by learn

--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -2,6 +2,7 @@ describe String do
   describe "#sentence?" do 
     it "returns true if the string that you are calling this method on ends in a period." do 
       expect("Hi, my name is Sophie.".sentence?).to eq(true)
+      binding.pry
     end
 
     it "returns false if the string that you are calling this method on does NOT end in a period." do 
